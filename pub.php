9fd139c5eadbaa9fa343cabcc5a7b919eb77e4f0
nut065 <57294663+nut065@users.noreply.github.com>
2020-03-24 14:58:51 +0700

    Create pub.php

---
+++ pub.php
@@ -0,0 +1,51 @@
+<?php
+ function pubMqtt($topic,$msg){
+       $APPID= "NeoSmartHome/"; //enter your appid
+     $KEY = "AFvHQLf6LbwevFCGTd1abGDK9irQhyPx"; //enter your key
+    $SECRET = "9@goMj)dusQ~eh@e8DsGuT#Evv#Ep8z7"; //enter your secret
+    $Topic = "$topic"; 
+      put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
+ 
+  }
+ function getMqttfromlineMsg($Topic,$lineMsg){
+ 
+    $pos = strpos($lineMsg, ":");
+    if($pos){
+      $splitMsg = explode(":", $lineMsg);
+      $topic = $splitMsg[0];
+      $msg = $splitMsg[1];
+      pubMqtt($topic,$msg);
+    }else{
+      $topic = $Topic;
+      $msg = $lineMsg;
+      pubMqtt($topic,$msg);
+    }
+  }
+ 
+  function put($url,$tmsg)
+{
+      
+    $ch = curl_init($url);
+ 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+     
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+     
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
+ 
+    //curl_setopt($ch, CURLOPT_USERPWD, "mJ7K4MfteC7p0dW:pp4gzMhCvJIqlxc66hKEvk46m");
+     
+    $response = curl_exec($ch);
+    
+      curl_close($ch);
+      echo $response . "\r\n";
+    return $response;
+}
+// $Topic = "esp8266";
+ //$lineMsg = "CHECK";
+ //getMqttfromlineMsg($Topic,$lineMsg);
+?>
